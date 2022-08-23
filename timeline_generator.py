a1876f50b789e543d7cc59912b48d3e215c26c8e
David Leigh <dleigh@us.ibm.com>
2022-08-22 21:50:39 -0400

    fix problem with nan

--- timeline_generator.py
+++ timeline_generator.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import textwrap
 
-
 def get_timeline(data, start=None, end=None,
                  interval=24, dateformat='%a %b %d', filename=None):
 
@@ -27,18 +26,20 @@ def get_timeline(data, start=None, end=None,
     data['options'] = data.apply(lambda row: set_defaults(row.options), axis=1)
     data_options = pd.DataFrame([x for x in data.options])
     data = data.combine_first(data_options)
+    data = data.where(pd.notnull(data), None)
 
     spans = data[data.end_datetime.notnull()]
-    ax.hlines(spans.height, spans.start_datetime, spans.end_datetime,
-              linewidth=spans.linewidth, capstyle='round', alpha=spans.alpha,
-              color=spans.color)
+    if spans.shape[0] > 0:
+        ax.hlines(spans.height, spans.start_datetime, spans.end_datetime,
+                  linewidth=spans.linewidth, capstyle='round', alpha=spans.alpha,
+                  color=spans.color)
     milestones = data[data.end_datetime.isnull()]
     vlines = milestones[milestones.vline == True]
     ax.plot(milestones.start_datetime, milestones.height, "o",
             color='darkblue', markerfacecolor="darkblue")
     ax.vlines(vlines.start_datetime, 0, vlines.height,
               color=vlines.color, linewidth=0.5)
-    data.apply(lambda row: annotate(ax,row), axis=1)
+    data.apply(lambda row: annotate(ax, row), axis=1)
 
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -63,9 +64,9 @@ def set_defaults(options):
         'horizontalalignment': 'left',
         'color': 'darkblue',
         'alpha': 1,
-        'linewidth':20,
+        'linewidth': 20,
         'vline': True
-    }     
+    }
     result = defaults
     for option in options:
         result[option] = options[option]
@@ -83,6 +84,7 @@ def annotate(ax, row):
         anchor = mdates.num2date(ax.get_xlim()[0])
     elif row['annotation_anchor'] == 'end':
         anchor = mdates.num2date(ax.get_xlim()[1])
+    print('arrowprops=',row.arrowprops)
     ax.annotate(description, xy=(anchor, row.height),
                 xytext=(row.x_offset, row.y_offset
                         ), textcoords="offset points",
